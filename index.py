ce8c00e839ef77a973cbced8e96699bf1c93d8d4
Vassu777 <Vasanth.vassu777@gmail.com>
2021-11-14 18:19:44 +0530

    i was added classes in this file and also inherited each other

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
-def add(a,b):
-    c=a+b
-    print(c)
-add(2,4)
+class operations():
+    c=12
+    def add(a,b):
+        c=a+b
+        print(c)
+        print('im adding classes')
+    add(2,4)
+
+class operations2(operations):
+
+    print(operations.c)
